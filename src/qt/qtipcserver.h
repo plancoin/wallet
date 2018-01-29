9610f6b35cba35861ad4dc33a41c0126bf8af133
Plancoin <support@plancoin.co>
2018-01-30 01:24:35 +0700

    Initialize

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define PLANCOIN-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "PLANCOINURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
